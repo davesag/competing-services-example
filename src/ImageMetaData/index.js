ff9e38fdf312b6f4b40be574ec177368d2484002
davesag <davesag@gmail.com>
2018-06-04 17:20:01 +0530

    n/a made the subscribers share a common generic base

--- src/ImageMetaData/index.js
+++ src/ImageMetaData/index.js
@@ -1,21 +1,5 @@
-const { makeSubscriber } = require('amqp-simple-pub-sub')
-
+const genericSubscriber = require('../genericSubscriber')
 const makeHandler = require('./makeHandler')
-const {
-  exchange,
-  routingKey,
-  META_DATA_QUEUE: queueName
-} = require('../constants')
+const { META_DATA_QUEUE: queueName } = require('../constants')
 
-const routingKeys = [routingKey]
-
-const makeService = () => makeSubscriber({ exchange, queueName, routingKeys })
-
-const start = async name => {
-  const service = makeService()
-  const handler = makeHandler(service, name)
-  await service.start(handler)
-  return service
-}
-
-module.exports = { start }
+module.exports = genericSubscriber(queueName, makeHandler)
--- test/unit/ImageMetaData/index_spec.js
+++ test/unit/ImageMetaData/index_spec.js
@@ -1,38 +1,21 @@
 const { expect } = require('chai')
 const sinon = require('sinon')
 const proxyquire = require('proxyquire')
-const { exchange } = require('../../../src/constants')
+const { META_DATA_QUEUE } = require('../../../src/constants')
 
 describe('ImageMetaData', () => {
-  const mockAMQP = {
-    makeSubscriber: sinon.stub()
-  }
-  const fakeSubscriber = {
-    start: sinon.stub()
-  }
-  const fakeService = 'a fake service'
+  const mockGenericSubscriber = sinon.spy()
+  const mockMakeHandler = sinon.spy()
 
-  const { start } = proxyquire('../../../src/ImageMetaData', {
-    'amqp-simple-pub-sub': mockAMQP
+  const imageMetaData = proxyquire('../../../src/ImageMetaData', {
+    '../genericSubscriber': mockGenericSubscriber,
+    './makeHandler': mockMakeHandler
   })
 
-  describe('start', () => {
-    before(async () => {
-      fakeSubscriber.start.resolves(fakeService)
-      mockAMQP.makeSubscriber.returns(fakeSubscriber)
-      await start('test')
-    })
-
-    it('called makeSubscriber', () => {
-      expect(mockAMQP.makeSubscriber).to.have.been.calledWith(
-        sinon.match({
-          exchange
-        })
-      )
-    })
-
-    it('called start', () => {
-      expect(fakeSubscriber.start).to.have.been.calledOnce
-    })
+  it('called genericSubscriber', () => {
+    expect(mockGenericSubscriber).to.have.been.calledWith(
+      META_DATA_QUEUE,
+      mockMakeHandler
+    )
   })
 })
